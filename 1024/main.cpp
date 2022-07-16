353bdcfa35ba8def19e10e8e545f3a641a3c25fb
carled7 <carlos_ed03@outlook.com>
2022-07-16 16:10:10 -0300

    problem 1024 solved

--- 1024/main.cpp
+++ 1024/main.cpp
@@ -8,14 +8,13 @@
 using namespace std;
 
 void encryptMessage(vector<string> msgs)
-{
+{    
 
     for (auto &&msg : msgs)
     {
-        if (msg == msgs[0])
-            continue;
 
-        char *encryptedMsg = (char *)malloc(msg.size() * sizeof(char));
+
+        char *encryptedMsg = (char *)malloc((msg.size() + 1) * sizeof(char));
         int halfMsg = msg.size() / 2 ;
 
         for (int i = 0; i < msg.size(); i++)
@@ -27,33 +26,18 @@ void encryptMessage(vector<string> msgs)
                 encryptedMsg[i] = int(msg[msg.size() - (i + 1)]);
 
         }
-       // printf("%s\n", encryptedMsg);
-        //cout << msg.size() << endl;
-        //cout << halfMsg << endl << endl;
+
         for (int i = halfMsg ; i < msg.size(); i++)
         {
 
             encryptedMsg[i] -= 1;
         }
+        encryptedMsg[msg.size()] = '\0';
 
         printf("%s\n", encryptedMsg);
-       // cout << halfMsg << endl;
     }
 }
-/*4
-Texto #3
-abcABC1
-vxpdylY .ph
-vv.xwfxo.fd
 
-3# rvzgV
-4
-1FECedc
-3
-ks. \nxfrzx
-5
-gi.r{hyz-xx
-5*/
 int main()
 {
 
@@ -62,17 +46,15 @@ int main()
     vector<string> messages;
 
     cin >> n;
+    cin.ignore();
 
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
         getline(cin, buffer);
         messages.push_back(buffer);
     }
     system("clear");
     encryptMessage(messages);
-    printf("\n");
 
     return 0;
 }
-
-//tentar descobrir como imprmir caracteres fora no range em c++ (ctrl shift u 1e)
